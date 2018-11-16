11f08a108b34a5b763aab66b4fd0158088c9afcc
asierhoras <asier.horas@opendeusto.es>
2018-11-17 00:41:21 +0100

    tabla de trackers

--- SDAproject/src/modelo/ViewThread.java
+++ SDAproject/src/modelo/ViewThread.java
@@ -31,7 +31,6 @@ public class ViewThread extends Thread{
 		while (true) {
 			try {
 				view.setTheMaster(myTracker.isMaster());
-
 				if (view.isTheMaster()) {
 					view.setTitle("Tracker Master "+myTracker.getID());
 				}else {
@@ -40,30 +39,27 @@ public class ViewThread extends Thread{
 				Thread.sleep(3000);
 
 				ArrayList<Integer> trackerList=myTracker.getTrackerList();
-				//trackerList.size();
-				//				Iterator<Integer> it = trackerList.iterator();
-				//				while(it.hasNext()){
-				//					int item=it.next();
-				//					System.out.println(String.valueOf(item));
-				//					System.out.println("ID: " + item);
-				//
-				//				}
+				Object[][] tableData=new Object[trackerList.size()][1];
+				for (int i=0;i<tableData.length;i++){
+					for(int j=0;j<tableData[0].length;j++){
+						tableData[i][j]=trackerList.get(i);
+					}
+				}
 				//view.setDataTabla1(new Object[][] { { myTracker.getID() }});
+				view.setDataTabla1(tableData);
+				view.setModelTrackers(new DefaultTableModel(view.getDataTabla1(), view.getColumnsTabla1()) {
+					@Override
+					public boolean isCellEditable(int row, int column) {
+						return false;
+					}
 
-				//final Class[] columnClassTabla1 = new Class[] { Integer.class, String.class, String.class, Boolean.class };
-				//		view.setModelTrackers(new DefaultTableModel(view.getDataTabla1(), view.getColumnsTabla1()) {
-				//			@Override
-				//			public boolean isCellEditable(int row, int column) {
-				//				return false;
-				//			}
-				//
-				//			//aqui falta columnclasstabla1
-				//		});
-				//
-				//		view.setTableTrackers(new JTable(view.getModelTrackers())); 
-				//		JScrollPane scrollPaneTabla1 = new JScrollPane(view.getTableTrackers());
-				//		scrollPaneTabla1.setBounds(32, 28, 452, 431);
-				//		view.getPanel2().add(scrollPaneTabla1);
+					//aqui falta columnclasstabla1
+				});
+
+				view.setTableTrackers(new JTable(view.getModelTrackers())); 
+				JScrollPane scrollPaneTabla1 = new JScrollPane(view.getTableTrackers());
+				scrollPaneTabla1.setBounds(32, 28, 452, 431);
+				view.getPanel2().add(scrollPaneTabla1);
 
 				view.repaint();
 			} catch (InterruptedException e) {
