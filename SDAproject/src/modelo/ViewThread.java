bb1fce238e4828b5cd588fbd7a4fa3bf07902b34
asierhoras <asier.horas@opendeusto.es>
2018-11-16 23:58:50 +0100

    Tema ventanas version no sé cuanto
    Funciona lo de los títulooos

--- SDAproject/src/modelo/ViewThread.java
+++ SDAproject/src/modelo/ViewThread.java
@@ -1,5 +1,13 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
 import vista.MainMenu;
 
 public class ViewThread extends Thread{
@@ -12,8 +20,58 @@ public class ViewThread extends Thread{
 
 	@Override
 	public void run() {
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		MainMenu view = new MainMenu(myTracker);
 		view.setVisible(true);
+		while (true) {
+			try {
+				view.setTheMaster(myTracker.isMaster());
+
+				if (view.isTheMaster()) {
+					view.setTitle("Tracker Master "+myTracker.getID());
+				}else {
+					view.setTitle("Tracker Slave "+myTracker.getID());
+				}
+				Thread.sleep(3000);
+
+				ArrayList<Integer> trackerList=myTracker.getTrackerList();
+				//trackerList.size();
+				//				Iterator<Integer> it = trackerList.iterator();
+				//				while(it.hasNext()){
+				//					int item=it.next();
+				//					System.out.println(String.valueOf(item));
+				//					System.out.println("ID: " + item);
+				//
+				//				}
+				//view.setDataTabla1(new Object[][] { { myTracker.getID() }});
+
+				//final Class[] columnClassTabla1 = new Class[] { Integer.class, String.class, String.class, Boolean.class };
+				//		view.setModelTrackers(new DefaultTableModel(view.getDataTabla1(), view.getColumnsTabla1()) {
+				//			@Override
+				//			public boolean isCellEditable(int row, int column) {
+				//				return false;
+				//			}
+				//
+				//			//aqui falta columnclasstabla1
+				//		});
+				//
+				//		view.setTableTrackers(new JTable(view.getModelTrackers())); 
+				//		JScrollPane scrollPaneTabla1 = new JScrollPane(view.getTableTrackers());
+				//		scrollPaneTabla1.setBounds(32, 28, 452, 431);
+				//		view.getPanel2().add(scrollPaneTabla1);
+
+				view.repaint();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
-	
+
 }
--- SDAproject/src/vista/MainMenu.java
+++ SDAproject/src/vista/MainMenu.java
@@ -37,11 +37,19 @@ public class MainMenu extends JFrame implements ActionListener {
 	private DefaultTableModel modelTrackers, modelSwarms;
 	private JTable tablePeers;
 	private Tracker myTracker;
+	private boolean isTheMaster;
+	private Object[][] dataTabla1;
+	private Class[] columnClassTabla1;
+	private JPanel panel2;
+	private String[] columnsTabla1;
+
+
+
 
 	public MainMenu(Tracker myTracker) {
 		super(); // usamos el contructor de la clase padre JFrame
-		configurarVentana(); // configuramos la ventana
 		this.myTracker = myTracker;
+		configurarVentana(); // configuramos la ventana
 		inicializarComponentes(); // inicializamos los atributos o componentes
 
 	}
@@ -49,7 +57,6 @@ public class MainMenu extends JFrame implements ActionListener {
 	private void configurarVentana() {
 		dim = super.getToolkit().getScreenSize();
 		super.setSize(dim);
-		this.setTitle("Main Menu");
 		this.setExtendedState(MAXIMIZED_BOTH);
 		getContentPane().setLayout(null);
 		this.setResizable(true);
@@ -100,7 +107,7 @@ public class MainMenu extends JFrame implements ActionListener {
 		tfID.setText(String.valueOf(myTracker.getID()));
 		panel1.add(tfID);
 
-		JButton btnIniciar = new JButton("Start/Stop");
+		JButton btnIniciar = new JButton("Stop");
 		btnIniciar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
@@ -130,27 +137,57 @@ public class MainMenu extends JFrame implements ActionListener {
 		panel1.add(btnIniciar);
 
 		// GESTOR DE TRACKERS (TAB2)
-		JPanel panel2 = new JPanel();
+		panel2 = new JPanel();
 		tabbedPane.addTab("Tracker gestor", null, panel2, null);
 		panel2.setLayout(null);
 
 		// headers for the table
-		String[] columnsTabla1 = new String[] { "Id", "Master or Slave", "Last Keepalive", "Active" };
+		columnsTabla1 = new String[] { "Id" };
+
+		
+		//		dataTabla1 = new Object[][] { { myTracker.getID(),isMaster , "1s", isActive }, { 2, "Slave", "1s", true },
+		//			{ 3, "Slave", "1s", true }, { 4, "Slave", "4s", false }, };
+		//
+		columnClassTabla1 = new Class[] { Integer.class };
+		//			modelTrackers = new DefaultTableModel(dataTabla1, columnsTabla1) {
+		//				@Override
+		//				public boolean isCellEditable(int row, int column) {
+		//					return false;
+		//				}
+		//
+		//				@Override
+		//				public Class<?> getColumnClass(int columnIndex) {
+		//					return columnClassTabla1[columnIndex];
+		//				}
+		//			};
+		//
+		//			tableTrackers = new JTable(modelTrackers);
+		//			JScrollPane scrollPaneTabla1 = new JScrollPane(tableTrackers);
+		//			scrollPaneTabla1.setBounds(32, 28, 452, 431);
+		//			panel2.add(scrollPaneTabla1);
+		// panel2.add(table);
+
+		JPanel panel3 = new JPanel();
+		panel3.setForeground(Color.BLACK);
+		tabbedPane.addTab("Gestor de Peers", null, panel3, null);
+		panel3.setLayout(null);
+
+		JLabel lblSwarmsActivos = new JLabel("Active Swarms");
+		lblSwarmsActivos.setFont(new Font("Consolas", Font.PLAIN, 33));
+		lblSwarmsActivos.setBounds(62, 50, 266, 37);
+		panel3.add(lblSwarmsActivos);
+
+		// headers for the table
+		String[] columnsTabla2 = new String[] { "Id", "Content name", "Size", "Number of seeders",
+		"Leechers" };
 
 		// actual data for the table in a 2d array
-		//Prueba muestra primer tracker
-		String isMaster;
-		boolean isActive=myTracker.isActive();
-		if(myTracker.isMaster()){
-			isMaster= "master";
-		}else{
-			isMaster="slave";
-		}
-		Object[][] dataTabla1 = new Object[][] { { myTracker.getID(),isMaster , "1s", isActive }, { 2, "Slave", "1s", true },
-			{ 3, "Slave", "1s", true }, { 4, "Slave", "4s", false }, };
+		Object[][] dataTabla2 = new Object[][] { { 1, "aaaaa", 1231, 4, 8 }, { 2, "bbbbb", 1233, 5, 9 },
+			{ 3, "ccccc", 4244, 6, 10 }, { 4, "ddddd", 5654, 7, 11 }, };
 
-			final Class[] columnClassTabla1 = new Class[] { Integer.class, String.class, String.class, Boolean.class };
-			modelTrackers = new DefaultTableModel(dataTabla1, columnsTabla1) {
+			final Class[] columnClassTabla2 = new Class[] { Integer.class, String.class, Integer.class, Integer.class,
+					Integer.class };
+			modelSwarms = new DefaultTableModel(dataTabla2, columnsTabla2) {
 				@Override
 				public boolean isCellEditable(int row, int column) {
 					return false;
@@ -158,126 +195,88 @@ public class MainMenu extends JFrame implements ActionListener {
 
 				@Override
 				public Class<?> getColumnClass(int columnIndex) {
-					return columnClassTabla1[columnIndex];
+					return columnClassTabla2[columnIndex];
 				}
 			};
 
-			tableTrackers = new JTable(modelTrackers);
-			JScrollPane scrollPaneTabla1 = new JScrollPane(tableTrackers);
-			scrollPaneTabla1.setBounds(32, 28, 452, 431);
-			panel2.add(scrollPaneTabla1);
-			// panel2.add(table);
+			JList swarmList = new JList();
+			swarmList.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			swarmList.setModel(new AbstractListModel() {
+				String[] values = new String[] {"Swarm1", "Swarm2", "Swarm3", "Swarm4"};
+				public int getSize() {
+					return values.length;
+				}
+				public Object getElementAt(int index) {
+					return values[index];
+				}
+			});
+			swarmList.setBounds(62, 116, 254, 387);
+			panel3.add(swarmList);
 
-			JPanel panel3 = new JPanel();
-			panel3.setForeground(Color.BLACK);
-			tabbedPane.addTab("Gestor de Peers", null, panel3, null);
-			panel3.setLayout(null);
-
-			JLabel lblSwarmsActivos = new JLabel("Active Swarms");
-			lblSwarmsActivos.setFont(new Font("Consolas", Font.PLAIN, 33));
-			lblSwarmsActivos.setBounds(62, 50, 266, 37);
-			panel3.add(lblSwarmsActivos);
-
-			// headers for the table
-			String[] columnsTabla2 = new String[] { "Id", "Content name", "Size", "Number of seeders",
-			"Leechers" };
-
-			// actual data for the table in a 2d array
-			Object[][] dataTabla2 = new Object[][] { { 1, "aaaaa", 1231, 4, 8 }, { 2, "bbbbb", 1233, 5, 9 },
-				{ 3, "ccccc", 4244, 6, 10 }, { 4, "ddddd", 5654, 7, 11 }, };
-
-				final Class[] columnClassTabla2 = new Class[] { Integer.class, String.class, Integer.class, Integer.class,
-						Integer.class };
-				modelSwarms = new DefaultTableModel(dataTabla2, columnsTabla2) {
-					@Override
-					public boolean isCellEditable(int row, int column) {
-						return false;
+			tableSwarms = new JTable(new DefaultTableModel(
+					new Object[][] {
+						{new Integer(1), "aaaaa", new Integer(1231), new Integer(4), new Integer(8)},
+					},
+					new String[] {
+							"Id", "Content name", "Size", "Seeders", "Leechers"
 					}
+					));
+			JScrollPane scrollPaneTabla2 = new JScrollPane(tableSwarms);
+			scrollPaneTabla2.setBounds(595, 148, 726, 43);
+			panel3.add(scrollPaneTabla2);
 
-					@Override
-					public Class<?> getColumnClass(int columnIndex) {
-						return columnClassTabla2[columnIndex];
+			JScrollPane scrollPaneTabla3 = new JScrollPane();
+			scrollPaneTabla3.setBounds(595, 309, 726, 347);
+			panel3.add(scrollPaneTabla3);
+
+			tablePeers = new JTable();
+			scrollPaneTabla3.setViewportView(tablePeers);
+			tablePeers.setModel(new DefaultTableModel(
+					new Object[][] {
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+						{null, null, null, null, null},
+					},
+					new String[] {
+							"ID", "ip", "Port", "Bytes pending", "Downloaded Bytes"
 					}
+					) {
+				Class[] columnTypes = new Class[] {
+						Integer.class, String.class, Integer.class, Double.class, Double.class
 				};
+				public Class getColumnClass(int columnIndex) {
+					return columnTypes[columnIndex];
+				}
+			});
 
-				JList swarmList = new JList();
-				swarmList.setFont(new Font("Tahoma", Font.PLAIN, 20));
-				swarmList.setModel(new AbstractListModel() {
-					String[] values = new String[] {"Swarm1", "Swarm2", "Swarm3", "Swarm4"};
-					public int getSize() {
-						return values.length;
-					}
-					public Object getElementAt(int index) {
-						return values[index];
-					}
-				});
-				swarmList.setBounds(62, 116, 254, 387);
-				panel3.add(swarmList);
+			JLabel lblPeersInfo = new JLabel("Peers Info");
+			lblPeersInfo.setFont(new Font("Tahoma", Font.PLAIN, 25));
+			lblPeersInfo.setBounds(595, 250, 121, 48);
+			panel3.add(lblPeersInfo);
 
-				tableSwarms = new JTable(new DefaultTableModel(
-						new Object[][] {
-							{new Integer(1), "aaaaa", new Integer(1231), new Integer(4), new Integer(8)},
-						},
-						new String[] {
-								"Id", "Content name", "Size", "Seeders", "Leechers"
-						}
-						));
-				JScrollPane scrollPaneTabla2 = new JScrollPane(tableSwarms);
-				scrollPaneTabla2.setBounds(595, 148, 726, 43);
-				panel3.add(scrollPaneTabla2);
-
-				JScrollPane scrollPaneTabla3 = new JScrollPane();
-				scrollPaneTabla3.setBounds(595, 309, 726, 347);
-				panel3.add(scrollPaneTabla3);
-
-				tablePeers = new JTable();
-				scrollPaneTabla3.setViewportView(tablePeers);
-				tablePeers.setModel(new DefaultTableModel(
-						new Object[][] {
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-							{null, null, null, null, null},
-						},
-						new String[] {
-								"ID", "ip", "Port", "Bytes pending", "Downloaded Bytes"
-						}
-						) {
-					Class[] columnTypes = new Class[] {
-							Integer.class, String.class, Integer.class, Double.class, Double.class
-					};
-					public Class getColumnClass(int columnIndex) {
-						return columnTypes[columnIndex];
-					}
-				});
-
-				JLabel lblPeersInfo = new JLabel("Peers Info");
-				lblPeersInfo.setFont(new Font("Tahoma", Font.PLAIN, 25));
-				lblPeersInfo.setBounds(595, 250, 121, 48);
-				panel3.add(lblPeersInfo);
-
-				JLabel lblSwarmInfo = new JLabel("Swarm Info");
-				lblSwarmInfo.setFont(new Font("Tahoma", Font.PLAIN, 25));
-				lblSwarmInfo.setBounds(595, 100, 135, 37);
-				panel3.add(lblSwarmInfo);
-				tablePeers.getColumnModel().getColumn(3).setPreferredWidth(97);
-				tablePeers.getColumnModel().getColumn(4).setPreferredWidth(105);
+			JLabel lblSwarmInfo = new JLabel("Swarm Info");
+			lblSwarmInfo.setFont(new Font("Tahoma", Font.PLAIN, 25));
+			lblSwarmInfo.setBounds(595, 100, 135, 37);
+			panel3.add(lblSwarmInfo);
+			tablePeers.getColumnModel().getColumn(3).setPreferredWidth(97);
+			tablePeers.getColumnModel().getColumn(4).setPreferredWidth(105);
 
 	}
 
@@ -307,6 +306,67 @@ public class MainMenu extends JFrame implements ActionListener {
 		}
 		return validation;
 	}
+
+	public Tracker getMyTracker() {
+		return myTracker;
+	}
+
+	public boolean isTheMaster() {
+		return isTheMaster;
+	}
+
+	public void setTheMaster(boolean isTheMaster) {
+		this.isTheMaster = isTheMaster;
+	}
+
+	public Object[][] getDataTabla1() {
+		return dataTabla1;
+	}
+
+	public void setDataTabla1(Object[][] dataTabla1) {
+		this.dataTabla1 = dataTabla1;
+	}
+
+	public Class[] getColumnClassTabla1() {
+		return columnClassTabla1;
+	}
+
+	public void setColumnClassTabla1(Class[] columnClassTabla1) {
+		this.columnClassTabla1 = columnClassTabla1;
+	}
+
+	public DefaultTableModel getModelTrackers() {
+		return modelTrackers;
+	}
+
+	public void setModelTrackers(DefaultTableModel modelTrackers) {
+		this.modelTrackers = modelTrackers;
+	}
+
+	public JTable getTableTrackers() {
+		return tableTrackers;
+	}
+
+	public void setTableTrackers(JTable tableTrackers) {
+		this.tableTrackers = tableTrackers;
+	}
+
+	public JPanel getPanel2() {
+		return panel2;
+	}
+
+	public void setPanel2(JPanel panel2) {
+		this.panel2 = panel2;
+	}
+	
+	public String[] getColumnsTabla1() {
+		return columnsTabla1;
+	}
+
+	public void setColumnsTabla1(String[] columnsTabla1) {
+		this.columnsTabla1 = columnsTabla1;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
