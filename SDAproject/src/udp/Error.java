b73e5a6585cfb1508de2c904f9b52066e6429e80
otxoa_95 <gochoadealda@opendeusto.es>
2018-12-10 19:30:00 +0100

    ERROR class with parse() and getBytes()

--- SDAproject/src/udp/Error.java
+++ SDAproject/src/udp/Error.java
@@ -1,5 +1,11 @@
 package udp;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+import udp.BitTorrentUDPMessage.Action;
+
 /**
  * 
  * Offset  Size            	Name            	Value
@@ -19,13 +25,35 @@ public class Error extends BitTorrentUDPMessage {
 	
 	@Override
 	public byte[] getBytes() {
-		//TODO: Complete this method
+		ByteBuffer buffer = ByteBuffer.allocate(16);
+		buffer.order(ByteOrder.BIG_ENDIAN);
+	
+		buffer.putInt(0, super.getAction().value());
+		buffer.putInt(4, super.getTransactionId());
+		buffer.position(8);
+		buffer.put(this.message.getBytes());
+		
+		buffer.flip();
 			
-		return null;
+		return buffer.array();
 	}
 	
 	public static Error parse(byte[] byteArray) {
-		//TODO: Complete this method
+		 try {
+		    	ByteBuffer buffer = ByteBuffer.wrap(byteArray);
+			    buffer.order(ByteOrder.BIG_ENDIAN);
+			    
+			    Error msg = new Error();
+			   
+			    msg.setAction(Action.valueOf(buffer.getInt(0)));	    
+			    msg.setTransactionId(buffer.getInt(4));
+			    msg.setMessage(Byte.toString(buffer.get(8)));
+			    
+			   
+				return msg;
+			} catch (Exception ex) {
+				System.out.println("# Error parsing Error message: " + ex.getMessage());
+			}
 		
 		return null;
 	}
